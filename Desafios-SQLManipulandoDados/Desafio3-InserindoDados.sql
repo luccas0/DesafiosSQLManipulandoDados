7cc3831eca2102b56b73f845171b81b4a0f8056c
Luccas Vergueiro <luccas.vergueiro@confitec.com.br>
2023-06-02 16:48:58 -0300

    Primeiro commit

---
+++ Desafios-SQLManipulandoDados/Desafio3-InserindoDados.sql
@@ -0,0 +1,13 @@
+INSERT INTO CLIENTES (
+CPF, NOME, ENDERECO,
+BAIRRO, CIDADE, ESTADO,
+CEP, DATA_NASCIMENTO, IDADE,
+GENERO, LIMITE_CREDITO, VOLUME_COMPRA,
+PRIMEIRA_COMPRA
+)
+VALUES ('1471156710', 'Érica Carvalho', 'R. Iriquitia', 'Jardins', 'São Paulo', 
+        'SP', '80012212', '19900901', 27, 'F', 170000, 24500,0), 
+    ('19290992743', 'Fernando Cavalcante', 'R. Dois de Fevereiro', 'Água Santa', 
+        'Rio de Janeiro', 'RJ', '22000000', '20000212', 18, 'M', 100000, 20000, 1), 
+    ('2600586709', 'César Teixeira', 'Rua Conde de Bonfim', 'Tijuca', 
+        'Rio de Janeiro', 'RJ', '22020001', '20000312', 18, 'M', 120000, 22000, 0);
---
+++ Desafios-SQLManipulandoDados/Desafio5-AlterandoDados.sql
@@ -0,0 +1,7 @@
+UPDATE CLIENTES SET 
+    ENDERECO = 'R. Jorge Emilio 23',
+    BAIRRO = 'Santo Amaro',
+    CIDADE = 'São Paulo',
+    ESTADO = 'SP',
+    CEP = '8833223'
+WHERE CPF = '19290992743';
